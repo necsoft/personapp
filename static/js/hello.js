03d2a40e8b52b2af160ee4995a1263291fef8761
Nec Necsoft <necsoftdesign@gmail.com>
2014-07-17 18:55:08 -0300

    Un poco de orden

--- static/js/hello.js
+++ static/js/hello.js
@@ -1,21 +1,10 @@
 var persona = require('tools/persona.js');
 var pdf_tools = require('tools/pdf_tools.js');
 
-$(document).ready(function() {
-    $("#get_pdf").click(function() {
-        persona.pedir(23, pdf_tools.create_pdf);
-    });
+// Click en el botón de crear PDF
+$("#get_pdf").click(function() {
+    // Pedimos el valor del input
+    var cantidad_paginas = $("#cantidad").val();
+    cantidad_paginas = parseInt(cantidad_paginas);
+    persona.pedir(cantidad_paginas, pdf_tools.create_pdf);
 });
-
-// function changeToLoad() {
-//     $("#main").load('views/loading.html');
-// }
-// 
-
-
-// function pdf_de_prueba() {
-//     var doc = new jsPDF();
-//     doc.addPage();
-//     doc.text(20, 20, "Nombre");
-//     doc.save("Test.pdf");
-// }
--- tools/pdf_tools.js
+++ tools/pdf_tools.js
@@ -2,13 +2,7 @@ var paginas;
 var doc = new window.jsPDF();
 
 exports.create_pdf = function(personas) {
-
-    // var hola = $("#main");
     paginas = personas.length;
-    console.log("Voy a hacer un PDF de: " + personas[0].nombre);
-    console.log("El PDF va a tener " + paginas + " paginas.");
-
-
     for (var i = 0; i < paginas; i++) {
         create_page(personas[i]);
         if (i === paginas - 1) {
@@ -18,9 +12,12 @@ exports.create_pdf = function(personas) {
 }
 
 function create_page(persona) {
-
     doc.text(20, 20, "Nombre: " + persona.nombre);
     doc.text(20, 30, "Apellido: " + persona.apellido);
     doc.text(20, 40, "Edad: " + persona.edad + " pirulos");
     doc.addPage();
+}
+
+function query_image() {
+
 }
