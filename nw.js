7ad2ad76a2220fc1e4195b74cf7e4b04a21dc1b7
Nec Necsoft <necsoftdesign@gmail.com>
2014-08-14 19:19:49 -0300

    Agregue los botones y la funcion de drag

--- nw.js
+++ nw.js
@@ -1,7 +1,7 @@
 //
 // app.js
 // -------------------------------------------------------
-// 
+//
 // Se encarga de hacer require de la gui de node-webkit.
 //
 
@@ -9,3 +9,19 @@
 var nw = window.require('nw.gui');
 //Creamos una variable para llamar a la ventana
 var win = nw.Window.get();
+//Comentar para no abrir la dev-tool
+win.showDevTools();
+
+//Aseguramos que este cargado todo el document
+$(document).ready(function(){
+
+  //Creamos el boton de minimizar
+  document.getElementById("minimize").onclick = function(){
+    win.minimize();
+  }
+
+  //Creamos el boton de close
+  document.getElementById("close").onclick = function(){
+    win.close();
+  }
+})
